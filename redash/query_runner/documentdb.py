b52bf04205b766f7cd3868012cd2b59efa01be96
Timothy Chung <tim5go@gmail.com>
2020-09-25 11:56:56 +0800

    use doucment id to replace natural ordering

--- redash/query_runner/documentdb.py
+++ redash/query_runner/documentdb.py
@@ -216,10 +216,10 @@ class DocumentDB(BaseQueryRunner):
             for d in db[collection_name].find().limit(2):
                 documents_sample.append(d)
         else:
-            for d in db[collection_name].find().sort([("$natural", 1)]).limit(1):
+            for d in db[collection_name].find().sort([("_id", 1)]).limit(1):
                 documents_sample.append(d)
 
-            for d in db[collection_name].find().sort([("$natural", -1)]).limit(1):
+            for d in db[collection_name].find().sort([("_id", -1)]).limit(1):
                 documents_sample.append(d)
         columns = []
         for d in documents_sample:
